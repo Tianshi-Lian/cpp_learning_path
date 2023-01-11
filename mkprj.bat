edb26a0f6200e0444b9313948bb83ed9b4aa4329
Rob Graham <r.ggraham@hotmail.com>
2023-01-11 19:37:50 +0000

    chore(tools): Repository & tools setup

---
+++ mkprj.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set folderName=%1
+
+IF NOT EXIST .\%1 mkdir .\%1
+
+pushd .\%1
+
+echo int main() > entry.cpp
+echo { >> entry.cpp
+echo     return 0; >> entry.cpp
+echo } >> entry.cpp
+
+echo g++ *.cpp -std=c++11 -Wall -o program > build.bat
+echo .\program > run.bat

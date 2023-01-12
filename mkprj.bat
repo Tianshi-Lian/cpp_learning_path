c7be4420d2316f2612220b0bd4df87bd6dd9454b
Rob Graham <r.ggraham@hotmail.com>
2023-01-12 20:15:41 +0000

    chore(tools): Fix template spacing

--- mkprj.bat
+++ mkprj.bat
@@ -6,10 +6,9 @@ IF NOT EXIST .\%1 mkdir .\%1
 
 pushd .\%1
 
-echo int main() > entry.cpp
-echo { >> entry.cpp
+echo int main() {> entry.cpp
 echo     return 0; >> entry.cpp
-echo } >> entry.cpp
+echo }>> entry.cpp
 
 echo g++ *.cpp -std=c++11 -Wall -o program > build.bat
 echo .\program > run.bat

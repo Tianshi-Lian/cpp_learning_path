bd0505233f644e7e5ff546fdf93cfd4103fffd51
Rob Graham <r.ggraham@hotmail.com>
2023-01-15 20:35:41 +0000

    feat(1_6): Completed 1_6

---
+++ 1_6/entry.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "sales_item.h"
+
+int main() {
+    // Exercise 1.25
+
+    Sales_Item total;
+    if (std::cin >> total) {
+        Sales_Item transaction;
+        while (std::cin >> transaction) {
+            if (transaction.isbn() == total.isbn()) {
+                total += transaction;
+            }
+            else {
+                std::cout << total << std::endl;
+                total = transaction;
+            }
+        }
+        std::cout << total << std::endl;
+    }
+    else {
+        std::cerr << "Failed to read first entry!" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}

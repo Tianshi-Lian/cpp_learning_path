756a42b6693aa7d0f3374ebe771fd465b4e76df6
unknown <r.ggraham@hotmail.com>
2023-01-28 22:21:56 +0000

    chore(build): Remove superfluous space from project generator

--- 1_cpp_primer/mkprj.bat
+++ 1_cpp_primer/mkprj.bat
@@ -7,7 +7,7 @@ IF NOT EXIST .\%1 mkdir .\%1
 pushd .\%1
 
 echo int main() {> entry.cpp
-echo     return 0; >> entry.cpp
+echo     return 0;>> entry.cpp
 echo }>> entry.cpp
 
 echo g++ *.cpp -std=c++11 -Wall -o program > build.bat

1a1e9940dab9eee4df8b0dd1d6f5bcc251983904
Rob Graham <r.ggraham@hotmail.com>
2023-02-02 20:09:29 +0000

    chore(cmake_02): Update project template

---
+++ cmake/_project_template/include/types.h
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+using u8 = uint32_t;
+using u16 = uint32_t;
+using u32 = uint32_t;
+using u64 = uint32_t;
+
+using i8 = int32_t;
+using i16 = int32_t;
+using i32 = int32_t;
+using i64 = int32_t;
+
+using f32 = float;
+using f64 = double;

b81fde8da95d72da08c4ead6b5b128f082550b04
Rob Graham <r.ggraham@hotmail.com>
2023-02-05 20:03:34 +0000

    feat(cmake_03): Complete basics/structure

---
+++ cmake/03_intro_to_modern_cmake/05_structure_projects/src/lib.cpp
@@ -0,0 +1,10 @@
+#include <project/lib.h>
+
+#include <tuple>
+#include <vector>
+#include <algorithm>
+
+std::tuple<double, double> accumulate_vector(const std::vector<double>& values) {
+    return std::make_tuple(5.0, 8.9);
+}
+

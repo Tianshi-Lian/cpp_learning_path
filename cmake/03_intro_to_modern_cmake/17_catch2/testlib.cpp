7ef47d012889cfd568c7bcc07adc6b51d0765243
Rob Graham <r.ggraham@hotmail.com>
2023-02-14 19:45:36 +0000

    feat(cmake_03): Complete testing/catch2

---
+++ cmake/03_intro_to_modern_cmake/17_catch2/testlib.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE("Quick check", "[main]") {
+    std::vector<double> values{1, 2., 3.0};
+    auto [mean, moment] = accumulate_vector(values);
+
+    REQUIRE(mean == 2.0);
+    REQUIRE(moment == Approx(4.6666666));
+}

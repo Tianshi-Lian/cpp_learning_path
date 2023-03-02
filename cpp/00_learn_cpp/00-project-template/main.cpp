75742a7584ec2f7c9c3ebdc8dae92f3b3b4a1337
Rob Graham <r.ggraham@hotmail.com>
2023-03-02 19:07:39 +0000

    refactor(learn_cpp): Refactor project-template to have callable sections for each section/exercise

--- cpp/00_learn_cpp/00-project-template/main.cpp
+++ cpp/00_learn_cpp/00-project-template/main.cpp
@@ -1,8 +1,36 @@
+#include <functional>
 #include <iostream>
+#include <span>
+#include <vector>
 
 int
-main()
+section_1()
 {
-    std::cout << "Hello, world!";
+    std::cout << "Hello, World!" << std::endl;
+
     return 0;
+}
+
+std::array<std::function<int()>, 1> sections = { section_1 };
+
+int
+main(int argc, char* argv[])
+{
+    auto args = std::span(argv, std::size_t(argc));
+
+    if (args.size() > 1) {
+        const auto section_to_run = (std::strtoull(args[1], nullptr, 10));
+        if (section_to_run > sections.size()) {
+            std::cerr << "Error, trying to run section: " << section_to_run << " which does not exist." << std::endl;
+            return -1;
+        }
+        else {
+            return std::invoke(sections.at(section_to_run - 1)); // Decrement for 0 indexing.
+        }
+    }
+    else {
+        for (auto& section : sections) {
+            std::invoke(section);
+        }
+    }
 }

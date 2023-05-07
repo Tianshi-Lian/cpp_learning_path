234ef83bb64c4d23f6bfc72be91b6af552047d86
Rob Graham <r.ggraham@hotmail.com>
2023-05-07 20:08:25 +0100

    feat(learn_cpp)[cpp_primer]: ch 3.4

--- cpp/01_cpp_primer/3_4/entry.cpp
+++ cpp/01_cpp_primer/3_4/entry.cpp
@@ -58,7 +58,60 @@ main()
         for (auto it = values.begin(); it != values.end(); ++it) {
             cout << *it << " ";
         }
+        cout << endl << endl;
     }
+
+    /**
+     * exercise 3.24
+     */
+    {
+        vector<int> list{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (auto it = list.begin(); it != list.end(); it += 2) {
+            cout << (*it + *(it + 1)) << '\n';
+        }
+        cout << endl;
+        for (auto fit = list.begin(), rit = list.end() - 1; rit > fit; ++fit, --rit) {
+            cout << (*fit + *rit) << '\n';
+        }
+        cout << endl << endl;
+    }
+
+    /**
+     * exercise 3.25
+     */
+    {
+        /**
+         * // count the number of grades by clusters of ten: 0--9, 10--19, . .. 90--99, 100
+         * vector<unsigned> scores(11, 0); // 11 buckets, all initially 0
+         * unsigned grade;
+         * while (cin >> grade) {
+         *  // read the grades
+         * if (grade <= 100)
+         *  // handle only valid grades
+         * ++scores[grade/10]; // increment the counter for the current cluster
+
+         */
+
+        vector<unsigned> buckets(11, 0);
+        vector<unsigned> scores{ 9, 12, 12, 15, 21, 30, 45, 47, 50, 71, 98, 93 };
+        for (auto it = scores.cbegin(); it != scores.cend(); ++it) {
+            ++buckets[*it / 10];
+        }
+
+        int bucket_val = 0;
+        for (auto it = buckets.begin(); it != buckets.end(); ++it, ++bucket_val) {
+            cout << (bucket_val * 10) << ": " << *it << endl;
+        }
+        cout << endl << endl;
+    }
+
+    /**
+     * exercise 3.26
+     *
+     * because beg, med and end are all iterators which mutate throughout processing and we need to ensure we're taking the
+     * difference between the adjusted iterator values
+     */
 
     return 0;
 }
